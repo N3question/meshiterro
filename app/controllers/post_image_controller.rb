5723e4afd33e1613c7fb0dd1b65107633a4934ce
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-01-22 07:13:29 +0000

    [add]10章・投稿機能の作成(3)_投稿できるようにしよう_確認まで

--- app/controllers/post_image_controller.rb
+++ app/controllers/post_image_controller.rb
@@ -1,10 +1,46 @@
 class PostImageController < ApplicationController
+  
+  ## Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+  ## NEXT Viewの作成
+    # Go_to app/views/post_images/new.html.erb
+        # コードを記述
   def new
+    @post_image = PostImage.new
   end
 
+  ## 投稿データを保存するためにcreateアクションを作成
+      #1 投稿するデータを PostImage モデルに紐づくデータとして保存する準
+          # フォームに入力されたデータ(shop_name や caption,image など)が、@post_image に格納
+      #2 @post_image(投稿データ)のuser_idを、current_user.id(今ログインしているユーザーの ID)に指定することで
+      #  投稿データに、今ログイン中のユーザーの ID を持たせる
+      #3 データをデータベースに保存するためのsaveメソッド実行
+      #4 リダイレクト先の指定
+  ## 同時にストロングパラメータも作成
+  ## NEXT ルーティングの追加
+    # resources に create を追加し完了
+  def create
+    @post_image = PostImage.new(post_image_params) #1
+    @post_image.user_id = current_user.id #2
+    @post_image.save #3
+    redirect_to post_images_path #4
+  end
+  
   def index
   end
 
   def show
+  end
+  
+  ## 投稿データのストロングパラメータ
+    # params
+        # formから送られてくるデータはparamsの中に入っている
+    # require
+        # 送られてきたデータの中からモデル名(ここでは:list)を指定し、データを絞り込み
+    # permit
+        # requireで絞り込んだデータの中から、保存を許可するカラムを指定
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,22 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   
-  # post_images コントローラの同じ名前のアクションに紐づく形で、以下のルーティングを自動生成
+  ## post_images コントローラの同じ名前のアクションに紐づく形で、以下のルーティングを自動生成
       # new, show, index, edit, create, update, destroy
       # onlyオプションを使用することで、生成するルーティングを限定
         # only の後に配列で記述されている"new","index","show"のアクション以外は、ルーティングが行われません。
         # 不要なルーティングが実行されないようにonlyを設定
       # ルーティングの確認
         # rails routes コマンドを実行
+  ## NEXT 投稿画面の作成
+    # Go_to app/controllers/post_image_controller.rb
+  ## 追記（createアクション作成後）=> :create
+    # NEXT ヘッダーに投稿フォームへのリンクを追加 
+      # Go_to app/views/layouts/application.html.erb
   # get 'post_image/new'
   # get 'post_image/index'
   # get 'post_image/show'
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   get 'homes/about'=>'homes#about' ,as:'about' # 追加。homes/aboutのルーティングに about ページを設定
   
